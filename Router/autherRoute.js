b7bae53d9b8eaf69f7d93f9960551c09ea7a274b
AhmedHussein000 <159377285+AhmedHussein000@users.noreply.github.com>
2024-08-08 02:37:20 +0300

    Delete Router directory

--- Router/autherRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getAuthers, createAuther } = require("../Controllers/autherController");
-
-const router = express.Router();
-
-router.route("/author").get(getAuthers).post(createAuther);
-
-module.exports = router;
