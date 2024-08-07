7d163050a686175a348d6dfeab9f1397daf3ba7f
ahmedhussein70 <ahmed_hussein201170@gmail.com>
2024-08-07 17:11:59 +0300

    add morgan lip

--- Backend/app.js
+++ Backend/app.js
@@ -1,14 +1,20 @@
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 const express = require("express");
 
 dotenv.config({ path: "config.env" });
 const app = express();
 
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log("Running");
+}
+
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log("Server running on port 8080");
 });
