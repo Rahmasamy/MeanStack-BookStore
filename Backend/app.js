fc18eef94b32fd96ac0672881a8a6aa3669ed6ab
ahmedhussein70 <ahmed_hussein201170@gmail.com>
2024-08-07 15:40:28 +0300

    upload angular project

---
+++ Backend/app.js
@@ -0,0 +1,14 @@
+const dotenv = require("dotenv");
+const express = require("express");
+
+dotenv.config({ path: "config.env" });
+const app = express();
+
+app.get("/api/auther", (req, res) => {
+  res.send({ auther: ["John Doe", "Jane Doe", "Jim Doe"] });
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log("Server running on port 8080");
+});
