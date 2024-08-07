2fd1005e7d29bf949618c86d961449252789c59a
Rahmasamy <rahmasamy949@gmail.com>
2024-08-07 22:52:08 +0300

    fix conflict

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+dotenv.config({ path: "config.env" });
+const dbConnection = require("./config/database");
+const autherRoute = require("./Router/autherRoute");
+
+// connect with db
+dbConnection();
+
+// express app
+const app = express();
+
+
+app.use(express.json());
+
+if (process.env.MODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log("Running"); 
+}
+
+// Routes
+
+app.use("/api/auther", autherRoute);
+
+// listen for changes and reload routes
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log("Server running on port 8080");
+});
