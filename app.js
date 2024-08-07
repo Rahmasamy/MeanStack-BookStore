a1ea08d840d12098d92798f57a997ef904d1b984
Rahmasamy <rahmasamy949@gmail.com>
2024-08-08 02:00:33 +0300

    Make Crud operations On Book

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
+// const HttpStatus=require('./utils/httpStatusText');
 dotenv.config({ path: "config.env" });
 const dbConnection = require("./config/database");
 const autherRoute = require("./Router/autherRoute");
-
+const bookRoute=require('./Router/bookRoute')
+const cors=require('cors');
 // connect with db
 dbConnection();
 
 // express app
 const app = express();
-
+app.use(cors())
 
 app.use(express.json());
 
@@ -22,10 +23,16 @@ if (process.env.MODE_ENV === "development") {
 
 // Routes
 
-app.use("/api/auther", autherRoute);
+app.use("/api/bookstore", autherRoute);
+app.use("/api/bookstore",bookRoute);
+// app.all('*',(req,res,next) => {
+//   return res.status(404).json({status:HttpStatus.ERROR,data:{course:"not valid url"}})
 
+// })
 // listen for changes and reload routes
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log("Server running on port 8080");
 });
+
+
